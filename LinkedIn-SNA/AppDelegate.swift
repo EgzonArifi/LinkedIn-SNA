b0ef4a89db5e67e9470519942da79c244c4bc2e3
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-12-23 11:36:17 +0100

    initial commit

---
+++ LinkedIn-SNA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkedIn-SNA
+//
+//  Created by Egzon Arifi on 12/23/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LinkedIn-SNA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LinkedIn-SNA
+//
+//  Created by Egzon Arifi on 12/23/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
