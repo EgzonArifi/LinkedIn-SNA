b84218f76817993ede8ee2fa479ca2a98f9b3b3b
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-12-30 19:48:50 +0100

    added matrix cell

---
+++ LinkedIn-SNA/MatrixCell.swift
@@ -0,0 +1,19 @@
+//
+//  MatrixCell.swift
+//  LinkedIn-SNA
+//
+//  Created by Egzon Arifi on 12/23/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Cocoa
+
+class MatrixCell: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
